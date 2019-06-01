98a816fe28bee4a8eecc379306c9d0cfce77319b
Adrian <mrduniak@icloud.com>
2019-06-01 23:03:52 +0200

    dodanie dostepu do bazy danych

--- src/main/java/akademia/storage/StorageApplication.java
+++ src/main/java/akademia/storage/StorageApplication.java
@@ -2,12 +2,13 @@ package akademia.storage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource("classpath:dbconfig.properties")
 @SpringBootApplication
 public class StorageApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StorageApplication.class, args);
     }
-
 }
